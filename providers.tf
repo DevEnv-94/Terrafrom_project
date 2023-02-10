29a6d58dcac39e96d949aa1b996abbdeab6129f9
Juri <juri.golotov@icloud.com>
2023-02-10 16:02:04 +0200

    Added aws provider

--- providers.tf
+++ providers.tf
@@ -4,9 +4,19 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "2.26.0"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.53.0"
+    }
   }
 }
 
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }

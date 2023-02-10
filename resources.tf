3dd6095e964e82ff7fe88898dbce7e8870e1ba01
Juri <juri.golotov@icloud.com>
2023-02-10 16:03:24 +0200

    Added data source aws_route53_zone and resource aws_route53_record

--- resources.tf
+++ resources.tf
@@ -15,3 +15,15 @@ resource "digitalocean_droplet" "droplet" {
   ssh_keys      = [digitalocean_ssh_key.ssh_key.fingerprint,data.digitalocean_ssh_key.rebrain_ssh_key.fingerprint]
   tags          = var.droplet_tags
 }
+
+data "aws_route53_zone" "dns_zone" {
+  name = var.dns_zone_name
+}
+
+resource "aws_route53_record" "domain" {
+  zone_id = data.aws_route53_zone.dns_zone.zone_id
+  name    = "${var.sub_domain}.${data.aws_route53_zone.dns_zone.name}"
+  type    = "A"
+  ttl     = 300
+  records = [digitalocean_droplet.droplet.ipv4_address]
+}

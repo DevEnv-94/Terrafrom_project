62d330e85b28f7ba698fe1966cc0d94c1a459938
Juri <juri.golotov@icloud.com>
2023-02-16 23:19:24 +0200

    Changed field are named records in resource aws_route53_record

--- resources.tf
+++ resources.tf
@@ -22,8 +22,8 @@ data "aws_route53_zone" "dns_zone" {
 
 resource "aws_route53_record" "domain" {
   zone_id = data.aws_route53_zone.dns_zone.zone_id
-  name    = "${var.sub_domain}.${data.aws_route53_zone.dns_zone.name}"
+  name    = "${var.sub_domain}"
   type    = "A"
   ttl     = 300
-  records = [digitalocean_droplet.droplet.ipv4_address]
+  records = [local.droplet_public_ip]
 }

3258097f6e904c2943487cb5960225da6426b18e
Juri <juri.golotov@icloud.com>
2023-02-20 14:57:21 +0200

    Added password output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
+output "my_domain_name" {
+  value = aws_route53_record.domain[*].fqdn
+}
+
 output "public_ip" {
   value = digitalocean_droplet.droplet[*].ipv4_address
 }
 
-output "my_domain_name" {
-  value = aws_route53_record.domain[*].fqdn
+output "Password" {
+  value = random_string.password[*].result
 }

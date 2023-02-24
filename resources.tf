f9f16ea2bf01bb181037b7a9bf1e56ccb84535b4
Juri <juri.golotov@icloud.com>
2023-02-24 12:01:39 +0200

    Final state,added new variables and resource local_file, changed remote-exec to create new user with ssh_authorized file and sudo privilige

--- resources.tf
+++ resources.tf
@@ -3,22 +3,22 @@ data "digitalocean_ssh_key" "rebrain_ssh_key" {
 }
 
 resource "digitalocean_ssh_key" "ssh_key" {
-  name       = var.ssh_key_name
-  public_key = file(var.ssh_key_path)
+  name       = var.ssh_public_key_name
+  public_key = file(var.ssh_public_key_path)
 }
 
 resource "random_string" "password" {
-  count            = var.droplets_count
+  count            = length(var.devs)
   length           = 15
   special          = true
 }
 
 resource "digitalocean_droplet" "droplet" {
-  count         = var.droplets_count
-  image         = "ubuntu-20-04-x64"
-  name          = "node-${count.index+1}"
-  region        = "fra1"
-  size          = "s-1vcpu-1gb"
+  count         = length(var.devs)
+  image         = var.image_name 
+  name          = var.devs[count.index].prefix
+  region        = var.region_name 
+  size          = var.droplet_size 
   ssh_keys      = [digitalocean_ssh_key.ssh_key.fingerprint,data.digitalocean_ssh_key.rebrain_ssh_key.fingerprint]
   tags          = var.droplet_tags
 
@@ -31,7 +31,14 @@ resource "digitalocean_droplet" "droplet" {
 
   provisioner "remote-exec" {
     inline = [
-      "echo 'root:${random_string.password[count.index].result}' | chpasswd",
+      "useradd -s /bin/bash -m ${var.devs[count.index].your_login}",
+      "usermod -aG sudo ${var.devs[count.index].your_login}",
+      "echo '${var.devs[count.index].your_login}:${random_string.password[count.index].result}' | chpasswd",
+      "mkdir /home/${var.devs[count.index].your_login}/.ssh",
+      "cp /root/.ssh/authorized_keys /home/${var.devs[count.index].your_login}/.ssh/",
+      "chown -R ${var.devs[count.index].your_login}:${var.devs[count.index].your_login} /home/${var.devs[count.index].your_login}/.ssh",
+      "chmod 700 /home/${var.devs[count.index].your_login}/.ssh",
+      "chmod 600 /home/${var.devs[count.index].your_login}/.ssh/authorized_keys",
     ]
   }
 }
@@ -41,10 +48,15 @@ data "aws_route53_zone" "dns_zone" {
 }
 
 resource "aws_route53_record" "domain" {
-  count   = var.domain_count
+  count   = length(var.devs)
   zone_id = data.aws_route53_zone.dns_zone.zone_id
-  name    = "${var.sub_domain}-${count.index+1}"
+  name    = "${var.devs[count.index].your_login}-${var.devs[count.index].prefix}"
   type    = "A"
   ttl     = 300
-  records = [local.droplets_public_ip[count.index]]
+  records = [digitalocean_droplet.droplet[count.index].ipv4_address]
+}
+
+resource "local_file" "example" {
+  content  = local.file_content
+  filename = "credentials.txt"
 }
